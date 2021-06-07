d0e1feb08463c972956bb88a3eab0286551abcea
alibhuiyan623 <alibhuiyan623@gmail.com>
2021-06-07 13:26:13 +0600

    added all

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Header from './compoments/Header/Header';
+import Shop from './compoments/Shop/Shop';
 
 function App() {
   return (
     <div >
-      
+      <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
